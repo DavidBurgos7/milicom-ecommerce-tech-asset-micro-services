8acfd787a1328c702d348bf23b77d350eafa74c1
David Burgos <dburgos834@gmail.com>
2025-05-21 15:37:23 -0600

    (init): adding notification service

---
+++ notification_service/src/test/java/com/burgos/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burgos.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
